63d1beb9542e16839a001adccfc3a2ee02301f36
JP Carey <jpcarey4@gmail.com>
2021-01-27 11:32:09 -0700

    run toggle on complete of todos

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { toggleToDo } from '../actions';
+import { connect } from 'react-redux';
 
-const ToDo = ({ todo }) => {
+const ToDo = ({ id, todo, completed, toggleToDo }) => {
   return (
-    <li>{todo}</li>
+    <li 
+      className={completed ? 'completed' : 'not-completed'} 
+      onClick={() => toggleToDo(id)}
+    >
+    {todo}
+  </li>
   )
 }
 
-export default ToDo;
+const mapDispatchToProps = dispatch => ({
+  toggleToDo: id => dispatch( toggleToDo(id) )
+})
+
+export default connect(null, mapDispatchToProps)(ToDo);
